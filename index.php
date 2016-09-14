19fe8e10580e946d4c366a9035f6ec891d98c491
DemienD <d.drost98@gmail.com>
2016-09-14 12:26:32 +0200

    Fix

--- index.php
+++ index.php
@@ -1,38 +1,5 @@
 <?php
-  define('TITLE' ,'test');
-  $nav = true;
-  $GLOBALS['MAIN_NAVIGATION'] = 'test';
-  $GLOBALS['INTRO_HEAD'] = 'test';
-  $GLOBALS['INTRO_PARA'] = 'test';
-  $GLOBALS['SECTORS'] = 'test';
-  $GLOBALS['COLUMN_1_BUTTON'] = 'test';
-  $GLOBALS['INTRO_HEAD_2'] = 'test';
-  $GLOBALS['INTRO_PARA_3'] = 'test';
-  $GLOBALS['INTRO_STEPS_HEAD_1'] = 'test';
-  $GLOBALS['INTRO_STEPS_1'] = 'test';
-  $GLOBALS['INTRO_STEPS_HEAD_2'] = 'test';
-  $GLOBALS['INTRO_STEPS_2'] = 'test';
-  $GLOBALS['INTRO_STEPS_HEAD_3'] = 'test';
-  $GLOBALS['INTRO_STEPS_3'] = 'test';
-  $GLOBALS['INTRO_STEPS_HEAD_4'] = 'test';
-  $GLOBALS['INTRO_STEPS_4'] = 'test';
-  $GLOBALS['INTRO_STEPS_HEAD_5'] = 'test';
-  $GLOBALS['INTRO_STEPS_5'] = 'test';
-  $GLOBALS['COLUMN_2_BUTTON'] = 'test';
-  $GLOBALS['INTRO_HEAD_3'] = 'test';
-  // define('INTRO_PARA_3' ,'test');
-  $GLOBALS['INTRO_LINKS'] = 'test';
-  $GLOBALS['QUOTE_L'] = 'test';
-  $GLOBALS['QOUTE_R'] = 'test';
-  $GLOBALS['QOUTE_AUTHOR'] = 'test';
-  $GLOBALS['TEMPLATES_HEAD'] = 'test';
-  $GLOBALS['TEMPLATES_PARA'] = 'test';
-  $GLOBALS['TEMPLATE_CATAGORIES'] = 'test';
-  $GLOBALS['TEMPLATES_BUTTON'] = 'test';
-  $GLOBALS['FEEDBACK_L'] = 'test';
-  $GLOBALS['FEEDBACK_R'] = 'test';
-  $GLOBALS['FEEDBACK_AUTHOR'] = 'test';
+  require_once '../inc/includes.php';
+
   include 'views/index.php';
-
-
 ?>
