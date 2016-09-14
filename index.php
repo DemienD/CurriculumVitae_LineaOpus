339e1b9249e1fbdf951d6c79b2da8aa5e2285400
DemienD <d.drost98@gmail.com>
2016-09-14 11:42:15 +0200

    MILESTONE, we actually see the page with false information

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+  define('TITLE' ,'test');
+  $nav = true;
+  define('MAIN_NAVIGATION' ,'test');
+  define('INTRO_HEAD' ,'test');
+  define('INTRO_PARA' ,'test');
+  define('SECTORS' ,'test');
+  define('COLUMN_1_BUTTON' ,'test');
+  define('INTRO_HEAD_2' ,'test');
+  define('INTRO_PARA_3' ,'test');
+  define('INTRO_STEPS_HEAD_1' ,'test');
+  define('INTRO_STEPS_1' ,'test');
+  define('INTRO_STEPS_HEAD_2' ,'test');
+  define('INTRO_STEPS_2' ,'test');
+  define('INTRO_STEPS_HEAD_3' ,'test');
+  define('INTRO_STEPS_3' ,'test');
+  define('INTRO_STEPS_HEAD_4' ,'test');
+  define('INTRO_STEPS_4' ,'test');
+  define('INTRO_STEPS_HEAD_5' ,'test');
+  define('INTRO_STEPS_5' ,'test');
+  define('COLUMN_2_BUTTON' ,'test');
+  define('INTRO_HEAD_3' ,'test');
+  // define('INTRO_PARA_3' ,'test');
+  define('INTRO_LINKS' ,'test');
+  define('QUOTE_L' ,'test');
+  define('QOUTE_R' ,'test');
+  define('QOUTE_AUTHOR' ,'test');
+  define('TEMPLATES_HEAD' ,'test');
+  define('TEMPLATES_PARA' ,'test');
+  define('TEMPLATE_CATAGORIES' ,'test');
+  define('TEMPLATES_BUTTON' ,'test');
+  define('FEEDBACK_L' ,'test');
+  define('FEEDBACK_R' ,'test');
+  define('FEEDBACK_AUTHOR' ,'test');
+
+  include 'views/index.php';
+
+?>
