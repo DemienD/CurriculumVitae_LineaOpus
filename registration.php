5c5e7869f3e48d89a233a635225baf1616fefcfa
Jolien Ribbers <jribbers1115@student.landstede.nl>
2016-09-15 23:48:32 +1000

    Registration beginsels

---
+++ registration.php
@@ -0,0 +1,41 @@
+<?php
+  require_once '/inc/connection.php';
+?>
+<?php
+  $areSet = [
+    'username'    => false,
+    'email'       => false,
+    'password'    => false
+  ]
+
+  if(isSet($_POST['username'])){
+    $areSet['username'] = $_POST['username'];
+  } else {
+    $error .= 'Username is a required field, please fill it out.';
+  }
+
+  if(isSet($_POST['email'])){
+    $areSet['email'] = $_POST['email'];
+  } else {
+    $error .= 'Email is a required field, please fill it out.';
+  }
+
+  if(isSet($_POST['password']) && $_POST['passwordConf'] === $_POST['password']){
+    $areSet['password'] = $_POST['password'];
+  } else {
+    $error .= 'Passwords do not match.';
+  }
+
+  $complete;
+  foreach($areSet as $key => $value){
+    if($value == false){
+      $complete == false
+    } else {
+      $complete = true;
+    }
+  }
+
+  if($complete == true){
+    // volgende stappen
+  }
+?>
--- views/registration.php
+++ views/registration.php
@@ -1,14 +1,14 @@
-<form action="" method="post">
-  <input type="text" name="username" placeholder="Username">
+<form action="registration.php" method="post">
+  <input type="text" name="username" placeholder="Username" required>
   <br />
   <br />
-  <input type="text" name="email" placeholder="Email">
+  <input type="text" name="email" placeholder="Email" required>
   <br />
   <br />
-  <input type="password" name="password" placeholder="Password">
+  <input type="password" name="password" placeholder="Password" required>
   <br />
   <br />
-  <input type="password" name="passwordConf" placeholder="Re-enter password">
+  <input type="password" name="passwordConf" placeholder="Re-enter password" required>
   <br />
   <br />
   <input type="submit" value="Submit">
