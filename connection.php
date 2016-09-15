dbc50e5301258480559b495c93cf264c58646551
Jolien Ribbers <jribbers1115@student.landstede.nl>
2016-09-15 20:23:34 +1000

    connection.php aangemaakt met connectie

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+  $connection = new PDO("mysql:host=localhost;dbname=cvcreate-server", "cvcreate-server", "6WqtJLHm827nB96Z");
+} catch (PDOexception $exception) {
+    echo "<script>alert('Connection to the database could not be made, please contact an administrator')</script>";
+    exit;
+}
+
+
+
+
+
+
+?>
