0e7cf4cb58a894b4965608e336501e97bed0e16f
Jolien Ribbers <jribbers1115@student.landstede.nl>
2016-09-15 21:59:08 +1000

    Beginsels registration.php + connection verhuisd naar /inc/

---
+++ views/registration.php
@@ -0,0 +1,15 @@
+<form action="" method="post">
+  <input type="text" name="username" placeholder="Username">
+  <br />
+  <br />
+  <input type="text" name="email" placeholder="Email">
+  <br />
+  <br />
+  <input type="password" name="password" placeholder="Password">
+  <br />
+  <br />
+  <input type="password" name="passwordConf" placeholder="Re-enter password">
+  <br />
+  <br />
+  <input type="submit" value="Submit">
+</form>
