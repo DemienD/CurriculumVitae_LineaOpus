d3fd0f8d73f800a4d2cb3af96e45b9b220a7e84d
Jolien Ribbers <jribbers1115@student.landstede.nl>
2016-09-19 18:42:16 +1000

    =='jes

--- registration.php
+++ registration.php
@@ -23,7 +23,7 @@
     }
 
     if(isSet($_POST['email'])){
-      if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === true){
+      if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == true){
         $areSet['email'] = $_POST['email'];
       } else {
         $error .= 'Email is invalid. Please enter a valid e-mail address.';
