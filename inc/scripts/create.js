5cec2937f5736aa18e40f8734364abc0773f2afc
DemienD <d.drost98@gmail.com>
2016-09-19 15:35:32 +0200

    JS to add more fields

--- inc/scripts/create.js
+++ inc/scripts/create.js
@@ -1,8 +1,17 @@
 $( document ).ready(function(){
 
-  $('#3').on('click', function() {
-    $('#3').before(form_3);
-    // console.log(form_3);
-  })
+  $( window ).on('click', function() {
+    if (typeof addButtons[0] != 'undefined') {
+      for(var i = 0; i < addButtons.length; i += 1) {
+        var id = '#' + addButtons[i];
+        $(id).on('click', function(event) {
+          console.log(event.target.id);
+          var form = 'form' + event.target.id;
+          console.log(form_sections[form]);
+          $('#'+event.target.id).before(form_sections[form]);
+        })
+      }
+    }
 
+  })
 })
--- views/create.php
+++ views/create.php
@@ -15,6 +15,8 @@
   </div>
   <div class="col-md-6">
       <?php
+      echo "<script> var form_sections = {}; var addButtons = [];</script>";
+
       if (isset($formSet) && $formSet == true) {
         echo '<form action="create.php" method="post">'."\n";
         $count = 0;
@@ -22,14 +24,14 @@
           echo "<hr />"."\n";
           echo "<h2>".$formSection[0][0]."</h2>";
           $count += 1;
-          $test = '';
+          $formSec = '';
           foreach (array_slice($formSection,1) as $name => $value) {
             $formCont = '';
+            if($formSection[0][1]){
+              $name = $name.'[]';
+            }
             switch ($value[1]) {
               case 'text':
-              if($formSection[0][1]){
-                $name = $name.'[]';
-              }
               $formCont .= '<input name="'.$name.'" id="'.$name.'"
               type="'.$value[1].'"
               placeholder="'.$value[0];
@@ -60,48 +62,18 @@
               break;
             }
             echo $formCont;
-            $test .= $formCont;
-          }
-          // foreach (array_slice($formSection,1) as $name => $value) {
-          //   $formCont;
-          //   switch ($value[1]) {
-          //     case 'text':
-          //     echo '<input name="'.$name.'" id="'.$name.'"
-          //     type="'.$value[1].'"
-          //     placeholder="'.$value[0];
-          //     if($value[2]){ echo '" required>'."\n"; } else {
-          //       echo '">'."\n";
-          //     };
-          //     break;
-          //     case 'select':
-          //     echo '<select id="'.$name.'" name="'.$name.'">'."\n";
-          //     echo '<option disabled selected>'.$value[0].'</option>'."\n";
-          //     foreach ($value[3] as $val) {
-          //       echo '<option value="'.$val.'">'.$val.'</option>'."\n";
-          //     }
-          //     echo '</select>';
-          //     break;
-          //     case 'range':
-          //     echo '<label for="'.$name.'">'.$value[0].'</label>';
-          //     echo '<input name="'.$name.'" id="'.$name.'"
-          //     type="'.$value[1].'"
-          //     min="0" max="10"
-          //     placeholder="'.$value[0];
-          //     if($value[2]){ echo '" required>'."\n"; } else {
-          //       echo '">'."\n";
-          //     };
-          //     break;
-          //     default:
-          //     # code...
-          //     break;
-          //   }
-          // }
-          if($formSection[0][1] == true) {
-            echo "<br /><h3 class='addForm' id='".$count."'>+ <small>Extra veld toevoegen</small></h3>";
+            $formSec .= $formCont;
           }
 
-          $test2 = trim(preg_replace('/\s+/', ' ', $test));
-          echo "<script> var form_".$count." = '".$test2."';</script>";
+          if($formSection[0][1] == true) {
+            echo "<br /><h3 class='addForm' id='".$count."'>+ <small>Extra veld toevoegen</small></h3>";
+            echo "<script>addButtons.push(".$count.");</script>";
+          }
+
+          $formConv = trim(preg_replace('/\s+/', ' ', $formSec));
+          echo "<script>
+            form_sections.form".$count." = '".$formConv."';
+          </script>";
         }
         echo '<input type="submit" name="saveCV" id="saveCV" value="CV opslaan">'."\n";
         echo '</form>';
