f5611472b5bff44d339626d136517d65d22d1b92
DemienD <d.drost98@gmail.com>
2016-09-23 09:26:50 +0200

    Printing the Concept-data

--- views/profile.php
+++ views/profile.php
@@ -16,20 +16,69 @@
         echo '<p>U heeft nog geen concept. U hebt een concept nodig om een CV te maken.</p>'."\n";
         echo '<a href="/"><button type="button" class="wideButton">Maak er nu een!</button></a>';
       } else {
+        echo "<ul class='hide'>";
         $content = $concept->concept;
-        print_r($content);
-        echo "<br /><br /><br /><br /><br /><br /><br /><br />";
+        // echo "<ul class='concept'>";
+
+        $prevHeader = 'none';
         foreach ($content as $key => $value) {
-          if(is_array($value)) {
-            echo $key . "<br />";
-            foreach ($value as $keyx => $valuex) {
-              echo $keyx . " | " . $valuex . "<br />";
+          if($key !== "saveCV") {
+            $indexName = explode( '_', $key );
+            $header;
+            switch ($indexName[0]) {
+              case 'personal':
+                $header = 'Persoonlijke Info';
+                break;
+              case 'education':
+                $header = 'Opleidingen';
+                break;
+              case 'work':
+                $header = 'Werkervaring';
+                break;
+              case 'language':
+                $header = 'Taalkennis';
+                break;
+              case 'program':
+                $header = 'Softwarekennis';
+                break;
+              case 'license':
+                $header = 'Rijbewijzen';
+                break;
+              case 'programming':
+                $header = 'Programmeertalen';
+                break;
+              case 'project':
+                $header = 'Projecten';
+                break;
+              default:
+                $header = '';
+                break;
             }
-          } else {
-            echo $key . " | " . $value . "<br />";
+
+            if($header !== $prevHeader) {
+              echo "</ul>";
+              echo "<ul class='concept'>";
+              echo "<h4>".$header."</h4>";
+              $prevHeader = $header;
+            }
+
+            if(is_array($value)) {
+              echo "<li class='key'>".$key;
+              echo "<ol>";
+
+              foreach ($value as $keyx => $valuex) {
+                echo "<li class='val'>" . $valuex . "</li>";
+              }
+              echo "</ol>";
+              echo "</li>";
+            } else {
+              echo "<li class='key'>".$key . "<li class='val'>" . $value . "</li></li>";
+            }
           }
         }
-        echo "<br />";
+
+        echo "</ul>";
+
       }
       ?>
   </div>
