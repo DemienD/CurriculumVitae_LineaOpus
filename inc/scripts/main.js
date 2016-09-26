d6fdf387f85254c5ed444b2eab086e6f353ae15a
Jolien Ribbers <jribbers1115@student.landstede.nl>
2016-09-26 21:52:40 +1000

    Logo fix smaller screens

--- inc/scripts/main.js
+++ inc/scripts/main.js
@@ -1,16 +1,18 @@
-$(document).ready(function(){
-
-  window.onresize = function(event) {
-    if(event.currentTarget.outerWidth >= 1080){
-        console.info("showing");
-        $("#notShow").show();
-    }else{
-        console.info("hiding");
-        $("#notShow").hide();
-    }
-
+// function for resizing(logo/title)
+function fixHeader() {
+  if(window.outerWidth >= 1080){
+    console.info("showing");
+    $("#notShow").show();
+  } else {
+    console.info("hiding");
+    $("#notShow").hide();
+  }
 }
 
+$(document).ready(function(){
+  fixHeader();
 
-
+  window.onresize = function(event) {
+    fixheader();
+  }
 });
