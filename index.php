cad3dfb4128c129bdbdb94237a1b6f77ec997c80
Jolien Ribbers <jribbers1115@student.landstede.nl>
2016-09-30 16:44:13 +0200

    beginsels

--- index.php
+++ index.php
@@ -7,10 +7,33 @@
   }
   $additionalCSS = ['landing'];
 
+  function establishConnection() {
+    try {
+      $connection = new PDO("mysql:host=localhost;dbname=cvcreate-server", "cvcreate-server", "6WqtJLHm827nB96Z");
+      return $connection;
+    } catch (PDOexception $exception) {
+        $this->message .= "Connection to the database could not be established.";
+        return false;
+        exit;
+    }
+  }
+
+  function addReview() {
+    $conn = $this->establishConnection();
+    if($conn !== false){
+
+    }
+  }
+
+
+
+
+
+
+
+
   $view = 'views/index.php';
 
   include $template;
-
-
 
 ?>
