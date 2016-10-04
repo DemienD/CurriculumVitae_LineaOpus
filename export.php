175c8e955f257449dfb71bb6983b4c774e7e1452
DemienD <d.drost98@gmail.com>
2016-10-04 15:27:11 +0200

    temp

---
+++ export.php
@@ -0,0 +1,25 @@
+<?php
+  /*include("inc/mpdf60/mpdf.php");
+
+  $resume = new mPDF();
+
+  $content = "
+    <style>
+      .sidebar {
+        width: 30%;
+        background-color: #0066aa;
+        height: 100%:
+        position: absolute;
+        left: 0;
+        top: 0;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+    <div class='sidebar'></div>
+  ";
+
+  $resume->WriteHTML($content);
+  $resume->Output();
+  exit;*/
+?>
